799bf75c8d5f2ed482b46b1140e61dd05ee03b97
Leandro Cruz <leandrocruz2000@gmail.com>
2025-12-04 17:25:55 -0300

    ProductManager funcional

--- index.js
+++ index.js
@@ -1,85 +1,102 @@
 const express = require('express')
-const app = express()
+const fs = require('fs').promises
 const port = 8080
 
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+class ProductManager {
+    constructor() {
+        this.productos = []
+        this.PRODUCTOS_FILE = 'products.json'
+    }
+
+    async getID() {
+        return (await this.consultaProductos()).length + 1
+    }
+
+    async consultaProductos() {
+        try {
+            const contenido = await fs.readFile(this.PRODUCTOS_FILE, 'utf-8')
+            return JSON.parse(contenido || '[]')
+        } catch (error) {
+            return []
+        }
+    }
+
+    async consultaProductosxId(id) {
+        const productoBuscado = await this.consultaProductos()
+        return productoBuscado.find(p => p.id === parseInt(id))
+    }
+
+    async agregarProducto(title, description, code, price, status, stock, category, thumbnails) {
+        const productos = await this.consultaProductos()
+        const nuevoProducto = { id: await this.getID(), title, description, code, price, status, stock, category, thumbnails };
+        productos.push(nuevoProducto)
+        await fs.writeFile(this.PRODUCTOS_FILE, JSON.stringify(productos))
+        console.log("El producto se ha agregado correctamente!");
+    }
+
+    async actualizarProducto(id, title, description, code, price, status, stock, category, thumbnails) {
+        const productos = await this.consultaProductos()
+        const producto = productos.find(p => p.id === parseInt(id))
+        if (!producto) { return res.status(404).json({ mensaje: "El producto no fue encontrado" }) }
+        producto.title = title || producto.title
+        producto.description = description || producto.description
+        producto.code = code || producto.code
+        producto.price = price || producto.price
+        producto.status = status || producto.status
+        producto.stock = stock || producto.stock
+        producto.category = category || producto.category
+        producto.thumbnails = thumbnails || producto.thumbnails
+        await fs.writeFile(this.PRODUCTOS_FILE, JSON.stringify(productos))
+        console.log("El Usuario se ha actualizado correctamente!");
+    }
+
+    async eliminarProducto(id) {
+        const productos = await this.consultaProductos();
+        const productoAEliminar = productos.filter(p => p.id !== Number(id));
+        await fs.writeFile(this.PRODUCTOS_FILE, JSON.stringify(productoAEliminar));
+        console.log("El Usuario se ha eliminado correctamente!");
+    }
+
+}
+
+const PM = new ProductManager()
 let productos = []
 
 //? Manejo de productos
-app.get('(/api/products/', (req, res) => { //Lista todos los productos
+app.get('/api/products', async (req, res) => { //Lista todos los productos
+    productos = await PM.consultaProductos()
     res.json(productos)
 })
 
-app.get('/api/products/:pid', (req, res) => { //Lista producto por ID
-    const { id } = req.params
-    const producto = productos.find(p => p.id === parseInt(id))
-    if (!producto) return res.status(404).json({ mensaje: "El producto no fue encontrado" })
-    res.json(producto)
-})
-
-app.post('/api/products', (req, res) => { // Crea un nuevo producto
-    const { title, description, code, price, status, stock, category, thumbnails } = req.body
-    const nuevoProducto = { id: productos.length + 1, title, description, code, price, status, stock, category, thumbnails }
-    productos.push(nuevoProducto)
-    res.status(201).json(nuevoProducto)
-})
-
-app.put('/api/products/:pid', (req, res) => { // Actualiza un producto por ID
-    const { id } = req.params
-    const { title, description, code, price, status, stock, category, thumbnails } = req.body
-    const producto = productos.find(p => p.id === parseInt(id))
-    if (!producto) return res.status(404).json({ mensaje: "El producto no fue encontrado" })
-    producto.title = title || producto.title
-    producto.description = description || producto.description
-    producto.code = code || producto.code
-    producto.price = price || producto.price
-    producto.status = status || producto.status
-    producto.stock = stock || producto.stock
-    producto.category = category || producto.category
-    producto.thumbnails = thumbnails || producto.thumbnails
-    res.json(producto)
-})
-
-app.delete('/api/products/:pid', (req, res) => { // Elimina un producto por ID
-    const { id } = req.params
-    productos = productos.filter(p => p.id !== parseInt(id))
-    res.json({ mensaje: "Producto eliminado" })
-})
-
-//? Manejo de carritos
-app.post('/api/carts', (req, res) => { // Crea carrito
-    const nuevoCarrito = { id: carrito.length + 1, products: [] }
-    carrito.push(nuevoCarrito)
-    res.status(201).json(nuevoCarrito)
-})
-
-app.get('/api/carts/:cid', (req, res) => { // Lista productos del carrito por ID
-    const { id } = req.params
-    const productoCarrito = carrito.find(p => p.id === parseInt(id))
-    if (!productoCarrito) return res.status(404).json({ mensaje: "El producto no fue encontrado en el carrito" })
-    res.json(productoCarrito)
-})
-
-app.post('/api/carts/:cid/product/:pid', (req, res) => { // Agrega un producto al carrito por ID
-    const { pid, cid } = req.params
-    const carritoEncontrado = carrito.find(c => c.id === parseInt(cid))
-    const producto = productos.find(p => p.id === parseInt(pid))
-
-    if (!carritoEncontrado) return res.status(404).json({ mensaje: "El carrito no fue encontrado" })
-    if (!producto) return res.status(404).json({ mensaje: "El producto no fue encontrado" })
-
-    // Busca si el producto ya existe en el carrito
-    const productoExiste = carritoEncontrado.products.find(p => p.product === parseInt(pid))
-
-    if (productoExiste) {
-        productoExiste.quantity += 1 // Si existe, incremento quantity
-    } else { // Si no existe, lo agrego
-        carritoEncontrado.products.push({
-            product: parseInt(pid),
-            quantity: 1
-        })
+app.get('/api/products/:pid', async (req, res) => { //Lista producto por ID
+    const { pid } = req.params
+    const producto = await PM.consultaProductosxId(pid)
+    if (!producto) {
+        return res.status(404).json({ mensaje: "Producto no encontrado" })
     }
-    res.json(carritoEncontrado)
+    res.json(producto)
+})
+
+app.post('/api/products', async (req, res) => { // Crea un nuevo producto
+    const { title, description, code, price, status, stock, category, thumbnails } = req.body
+    await PM.agregarProducto(title, description, code, price, status, stock, category, thumbnails)
+    res.status(201).json({ mensaje: "Producto creado" })
+})
+
+app.put('/api/products/:pid', async (req, res) => { // Actualiza un producto por ID
+    const { pid } = req.params
+    const { title, description, code, price, status, stock, category, thumbnails } = req.body
+    await PM.actualizarProducto(pid, title, description, code, price, status, stock, category, thumbnails)
+    res.json({ mensaje: "Producto actualizado" })
+})
+
+app.delete('/api/products/:pid', async (req, res) => { // Elimina un producto por ID
+    const { pid } = req.params
+    await PM.eliminarProducto(pid)
+    res.json({ mensaje: "Producto eliminado" })
 })
 
 app.listen(port, () => { // Inicia el servidor
